6ab66e2da19d32521e2856880e344cd27433efec
ivan_lane <ivanlane1991@gmail.com>
2023-03-09 22:05:59 +0300

    cmd_execve is modified

--- exec_command.c
+++ exec_command.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -10,39 +11,44 @@ void exec_command(char **tokens)
 {   
     pid_t pid = fork();
 
-if(pid != 0)
-{
-    wait(NULL);
-}
-else if(pid == 0)
-{    
-    char *path = getenv("PATH");
+    if(pid != 0)
+    {
+        wait(NULL);
+    }
+    else if(pid == 0)
+    {    
+        char *path = getenv("PATH");
 
-    char *path_token = strtok(path, ":");
-    int command_length = strlen(tokens[0]);
-    struct stat buffer;
+        char *path_token = strtok(path, ":");
+        int command_length = strlen(tokens[0]);
+        struct stat buffer;
 
-        while(path_token != NULL)
-        {
-            char *real_path = malloc(command_length + strlen(path_token) + 2);
-            // char *p = "\\bin\\ls";
-    
-            strcpy(real_path, path_token);
-            strcat(real_path, "/");
-            strcat(real_path, tokens[0]);
-            strcat(real_path, "\0");
+            while(path_token != NULL)
+            {
+                char *real_path = malloc(command_length + strlen(path_token) + 2);
+        
+                strcpy(real_path, path_token);
+                strcat(real_path, "/");
+                strcat(real_path, tokens[0]);
+                strcat(real_path, "\0");
 
-            if(stat(real_path, &buffer) == 0)
-            {   
-                // printf("real path: %s \n", real_path);
+                if(stat(real_path, &buffer) == 0)
+                {   
                     if(execve(real_path, tokens, NULL) == -1)
                     {
                         perror("ERROR:");
                     }
+                    free(real_path);
+                }
+
+                free(real_path);
+                path_token = strtok(NULL, ":");
+
+                if(path_token == NULL)
+                {   
+                    printf("command \"%s\" is not found \n", tokens[0]);
+                    exit(1);
+                }
             }
-            // printf("real path: %s \n", real_path);
-            free(real_path);
-            path_token = strtok(NULL, ":");
-        }
-}
+    }
 }
--- main.c
+++ main.c
@@ -16,48 +16,6 @@ void mem_free(char *str, char **tokens)
     free(tokens);
 }
 
-
-// void exec_command(char **tokens)
-// {   
-//     pid_t pid = fork();
-
-// if(pid != 0)
-// {
-//     wait(NULL);
-// }
-// else if(pid == 0)
-// {    
-//     char *path = getenv("PATH");
-
-//     char *path_token = strtok(path, ":");
-//     int command_length = strlen(tokens[0]);
-//     struct stat buffer;
-
-//         while(path_token != NULL)
-//         {
-//             char *real_path = malloc(command_length + strlen(path_token) + 2);
-//             // char *p = "\\bin\\ls";
-    
-//             strcpy(real_path, path_token);
-//             strcat(real_path, "/");
-//             strcat(real_path, tokens[0]);
-//             strcat(real_path, "\0");
-
-//             if(stat(real_path, &buffer) == 0)
-//             {   
-//                 // printf("real path: %s \n", real_path);
-//                     if(execve(real_path, tokens, NULL) == -1)
-//                     {
-//                         perror("ERROR:");
-//                     }
-//             }
-//             // printf("real path: %s \n", real_path);
-//             free(real_path);
-//             path_token = strtok(NULL, ":");
-//         }
-// }
-// }
-
 int main(int argc, char *argv)
 {   
     char *prompt = "(my_shell) $:";
