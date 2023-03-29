fde9f2e7d3020340f91a36bc5331176fd367c43a
ivan_lane <ivanlane1991@gmail.com>
2023-03-29 17:24:41 +0300

    start command_table

--- command_table.c
+++ command_table.c
@@ -4,9 +4,11 @@
 
 #include "token_struct.h"
 
-struct token_struct **command_table(char **tokens)
+struct command_str **command_table(char **tokens)
 {
-
-
+    
+    struct command_str **simple_command = malloc(sizeof(struct command_str)*2);
+    simple_command[0] = malloc(sizeof(struct command_str));
+    simple_command[0]->command = tokens;
 
 }
--- command_table.h
+++ command_table.h
@@ -1,7 +1,7 @@
 #ifndef COMMAND_STRUCT_H
 #define COMMAND_STRUCT_H
 
-struct token_struct **command_table(char **tokens);
+struct command_str **command_table(char **tokens);
 
 
 #endif
